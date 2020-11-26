33c6b7286586c16270071366ed96b1d3fa7fe504
cless <hectorgh@gmail.com>
2020-11-26 13:07:12 +0100

    downgraded version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == '__main__':
 	
 	setup(name='n4d',
-		version='0.999',
+		version='0.900',
 		description='N4d',
 		long_description="""""",
 		author='Hector Garcia Huerta',
