d1302a64b00c19cd09d1223c74989ba6b3e3922a
cless <hectorgh@gmail.com>
2020-11-30 09:27:02 +0100

    get methods is now somehow usable

--- built-in/get_methods.py
+++ built-in/get_methods.py
@@ -1,7 +1,14 @@
 import n4d.responses
+import n4d.server.core
 
 def get_methods(self,*args):
-	ret=("[CLASS] Function ...\n")
-	return n4d.responses.build_call_successful_response(ret,"Here's to you")
+	
+	core=n4d.server.core.Core.get_core()
+	ret={}
+	for x in core.plugin_manager.plugins:
+		if core.plugin_manager.plugins[x]["found"]:
+			ret[x]=core.plugin_manager.plugins[x]["methods"]
+	
+	return n4d.responses.build_call_successful_response(ret)
 
 #def test
