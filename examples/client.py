abae6fb6f0c3f9b82537100f56aab70d804192dd
cless <hectorgh@gmail.com>
2020-11-30 09:17:30 +0100

    fixed example

--- examples/client.py
+++ examples/client.py
@@ -1,72 +1,27 @@
 import xmlrpc.client
 import ssl
 import random
+import os
 context=ssl._create_unverified_context()
-s = xmlrpc.client.ServerProxy('https://localhost:9800',context=context,allow_none=True)
+s = xmlrpc.client.ServerProxy('https://localhost:9779',context=context,allow_none=True)
 
-key="oQNxVe6ZL06N6vq79LLFqHtCi3mtA8uDHTKCj52wVN0IwQpvl3"
-#ret=s.get_methods("","Core",1,2,3)
+user=os.environ["USER"]
 
-#ret=s.test(key,"TestPlugin",1,4)
-#ret=s.lliurex_version(key,"LliurexVersion")
-
-#ret=s.test(key,"TestPlugin",1,4)
-#print(ret)
-
-#ret=s.get_methods()
-#print(ret["return"])
-
-#ret=s.get_variable("SLAVE_BLACKLIST")
-#print(ret)
-
-'''
-ret=s.get_variables()
-
-for var in ret["return"]:
-	print("%s=%s"%(var,ret["return"][var]))
-print()
-
-ret=s.set_variable(key,"TRALARI",123)
+ret=s.create_ticket(user)
 print(ret)
 
-extra_info={}
-extra_info["comment"]="Molo mogollon"
-extra_info["volatile"]=True
-
-ret=s.set_variable(key,"TRALARO",1234,extra_info)
-print(ret)
-print()
-
-ret=s.get_variables(True)
-
-
-for var in ret["return"]:
-	print("%s=%s"%(var,ret["return"][var]))
-
-'''
-
-ret=s.create_ticket("cless")
-print(ret)
-
-f=open("/run/n4d/tickets/cless")
+f=open("/run/n4d/tickets/%s"%user)
 password=f.readline()
 f.close()
 
-
-'''
-ret=s.set_variable(("cless",password),"REMOTE_VARIABLES_SERVER","127.0.0.1")
+ret=s.set_variable((user,password),"TRALARI","127.0.0.1")
 print(ret)
-
-ret=s.get_variable("SRV_IP")
-print(ret)
-ret=s.delete_variable(("cless",password),"REMOTE_VARIABLES_SERVER")
-print(ret)
-
-'''
-
-#ret=s.set_variable(("cless",password),"REMOTE_VARIABLES_SERVER","127.0.0.1")
-#print(ret)
 
 ret=s.get_variables()
 print(ret)
 
+ret=s.delete_variable((user,password),"TRALARI")
+print(ret)
+
+ret=s.get_variables()
+print(ret)
