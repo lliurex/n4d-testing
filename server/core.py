1aa67bbfc2a9bc42c3246f6f6bc199abc71f6167
cless <hectorgh@gmail.com>
2020-12-14 13:41:12 +0100

    added allowed_users support

--- server/core.py
+++ server/core.py
@@ -659,10 +659,17 @@ class Core:
 				groups=["anonymous"]
 			
 			ok=False
+			
 			for group in groups:
 				if group in self.plugin_manager.plugins[n4d_call_data["class"]]["methods"][n4d_call_data["method"]]["allowed_groups"]:
 					ok=True
 					break
+					
+			if not ok:
+				if "allowed_users" in self.plugin_manager.plugins[n4d_call_data["class"]]["methods"][n4d_call_data["method"]]:
+					if user in self.plugin_manager.plugins[n4d_call_data["class"]]["methods"][n4d_call_data["method"]]["allowed_users"]:
+						ok=True
+						break
 		
 			if n4d_call_data["user"]=="root" and not ok:
 				ok=True
